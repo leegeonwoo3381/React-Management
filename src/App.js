135c19c725a905e2d8e5b965d8d35c70c94221d1
leegeonwoo <dlrjsdnzzzz@naver.com>
2022-06-29 16:58:29 +0900

    toc event

--- src/App.js
+++ src/App.js
@@ -28,21 +28,67 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      Subject:{title:'WEB',sub:'World Wide Web'},
+      mode:'read',
+      selected_content_id:2,
+      subject:{title:'WEB',sub:'World Wide Web'},
+      welcome:{title:'Welcome', desc:'Hello, React!!!'},
       contents:[
-        {id:1, title:'HTML',desc:'HTMO is for imformation'},
+        {id:1, title:'HTML',desc:'HTML is for imformation'},
         {id:2, title:'CSS',desc:'CSS is for design'},
         {id:3, title:'JavaScript', desc:'JavaScript is for interactive'},
       ]
     }
   }
   render() {
+    console.log('App render');
+    let _title, _desc = null;
+    if(this.state.mode === 'Welcome') {       // == 는 값이 같은 때만 사용, === 는 값과, 타입이 같은지 확인할 때 사용
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    }else if(this.state.mode === 'read') {
+      // for(let i=0; i<this.state.contents.length; i++) {
+      //   let data = this.state.contents[i];
+      //   if(data.id === this.setState.selected_content_id) {
+      //     _title = data.title;
+      //     _desc = data.desc;
+      //     break;
+        
+      //   }
+      // }
+      let i = 0;
+      while(i < this.state.contents.length) {
+        let data = this.state.contents[i];
+        if(data.id === this.state.selected_content_id) {
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i = i+1;
+      }
+
+    }
     return (
       <div>
-        <Subject title={this.state.Subject.title} sub={this.state.Subject.sub}></Subject>   {/* <Subject /> */}
+        
+        <Subject 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub}
+          onChangePage={function(){this.setState({mode:'Welcome'})}.bind(this)}
+        ></Subject>   {/* <Subject /> */}
+
         {/* <Subject title="React" sub="FOR UI"></Subject> */}
-        <TOC data={this.state.contents}></TOC>
-        <COL title="HTML" desc="HTML is HyperText Markup Language...."></COL>
+        <TOC data={this.state.contents}
+          onChangePage={function(id){
+            this.setState(
+              {
+                mode:'read',
+                selected_content_id:Number(id)
+              }
+              );
+          }.bind(this)}
+        ></TOC>
+
+        <COL title={_title} desc={_desc}></COL>
       </div>
       // <div className="App">
       //   <header className="App-header">
--- src/components/COL.js
+++ src/components/COL.js
@@ -2,6 +2,7 @@ import React,{ Component } from 'react';
 
 class COL extends Component {
     render() {
+      console.log('COL render');
       return (
         <article>
           <h2>{this.props.title}</h2>
