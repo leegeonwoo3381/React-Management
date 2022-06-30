35b9173cc86b09ac0a9ca1666c186f21e12dd968
leegeonwoo <dlrjsdnzzzz@naver.com>
2022-06-30 17:41:52 +0900

    create update.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 // // import logo from './logo.svg';
 // import './App.css';
 // import React,{ Component } from 'react';
-import TOC from './components/TOC';
-import ReadContent from './components/ReadContent';
-import Subject from './components/Subject';
-import Control from './components/Control';
-import UpdateContent from './components/UpdateContent';
+// import TOC from './components/TOC';
+// import ReadContent from './components/ReadContent';
+// import Subject from './components/Subject';
+// import Control from './components/Control';
+// import UpdateContent from './components/UpdateContent';
 
-import './App.css';
-import React,{ Component } from 'react';
-import CreateContent from './components/CreateContent';
+// import './App.css';
+// import React,{ Component } from 'react';
+// import CreateContent from './components/CreateContent';
 // import ReadContent from './components/ReadContent';
 // import Header from '../src/chrome/Header';
 // import Body from '../src/chrome/Body';
@@ -29,94 +29,115 @@ import CreateContent from './components/CreateContent';
 
 //--------------------------------------------------------------------------------------------------------------------------
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.max_content_id = 3;
-    this.state = {
-      mode:'read',
-      selected_content_id:2,
-      subject:{title:'WEB',sub:'World Wide Web'},
-      welcome:{title:'Welcome', desc:'Hello, React!!!'},
-      contents:[
-        {id:1, title:'HTML',desc:'HTML is for imformation'},
-        {id:2, title:'CSS',desc:'CSS is for design'},
-        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'},
-      ]
-    }
-  }
-  render() {
-    console.log('App render');
-    let _title, _desc = null, _article;
-    if(this.state.mode === 'Welcome') {       // == 는 값이 같은 때만 사용, === 는 값과, 타입이 같은지 확인할 때 사용
-      _title = this.state.welcome.title;
-      _desc = this.state.welcome.desc;
-      _article = <ReadContent title={_title} desc={_desc}></ReadContent>;
-    }else if(this.state.mode === 'read') {
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.max_content_id = 3;
+//     this.state = {
+//       mode:'read',
+//       selected_content_id:2,
+//       subject:{title:'WEB',sub:'World Wide Web'},
+//       welcome:{title:'Welcome', desc:'Hello, React!!!'},
+//       contents:[
+//         {id:1, title:'HTML',desc:'HTML is for imformation'},
+//         {id:2, title:'CSS',desc:'CSS is for design'},
+//         {id:3, title:'JavaScript', desc:'JavaScript is for interactive'},
+//       ]
+//     }
+//   }
+
+  //getReadContent Start
+
+  // getReadContent() {
+  //   let i = 0;
+  //     while(i < this.state.contents.length) {
+  //       let data = this.state.contents[i];
+  //       if(data.id === this.state.selected_content_id) {
+  //         return data;
+  //       }
+  //       i = i+1;
+  //     }
+  // }
+
+  //getReadContent End
+
+  //getContent Start
+  
+  // getContent() {
+  //   let _title, _desc = null, _article;
+  //   if(this.state.mode === 'Welcome') {       // == 는 값이 같은 때만 사용, === 는 값과, 타입이 같은지 확인할 때 사용
+  //     _title = this.state.welcome.title;
+  //     _desc = this.state.welcome.desc;
+  //     _article = <ReadContent title={_title} desc={_desc}></ReadContent>;
+  //   }else if(this.state.mode === 'read') {
+  //     let _content = this.getReadContent();
+
       // for(let i=0; i<this.state.contents.length; i++) {
       //   let data = this.state.contents[i];
       //   if(data.id === this.setState.selected_content_id) {
       //     _title = data.title;
       //     _desc = data.desc;
       //     break;
-        
       //   }
       // }
-      let i = 0;
-      while(i < this.state.contents.length) {
-        let data = this.state.contents[i];
-        if(data.id === this.state.selected_content_id) {
-          _title = data.title;
-          _desc = data.desc;
-          break;
-        }
-        i = i+1;
-      }
-      _article = <ReadContent title={_title} desc={_desc}></ReadContent>;
-    }else if(this.state.mode === 'create') {
-      _article = <CreateContent onSubmit={function(_title,_desc){
-        // console.log(_title, _desc);
-        this.max_content_id = this.max_content_id +1;
-        // this.state.contents.push(
-        //   {id:this.max_content_id,title:_title,desc:_desc}
-        // );
+      
+  //     _article = <ReadContent title={_content.title} desc={_content.desc}></ReadContent>;
+  //   }else if(this.state.mode === 'create') {
+  //     _article = <CreateContent onSubmit={function(_title,_desc){
+  //       // console.log(_title, _desc);
+  //       this.max_content_id = this.max_content_id +1;
+  //       // this.state.contents.push(
+  //       //   {id:this.max_content_id,title:_title,desc:_desc}
+  //       // );
+  
+  //       let _contents = this.state.contents.concat(
+  //         {id:this.max_content_id,title:_title,desc:_desc}
+  //       );
+  
+  //       this.setState({
+  //         contents : _contents
+  //       })
+  //     }.bind(this)}></CreateContent>
+  //   }else if(this.state.mode === 'update') {
+  //     let _content = this.getReadContent();
+  //     _article = <UpdateContent data = {_content}></UpdateContent>
+  //   }
 
-        let _contents = this.state.contents.concat(
-          {id:this.max_content_id,title:_title,desc:_desc}
-        );
 
-        this.setState({
-          contents : _contents
-        })
-      }.bind(this)}></CreateContent>
-    }
-    return (
-      <div>
-        
-        <Subject 
-          title={this.state.subject.title} 
-          sub={this.state.subject.sub}
-          onChangePage={function(){this.setState({mode:'Welcome'})}.bind(this)}
-        ></Subject>   {/* <Subject /> */}
+  //   return _article;
 
-        {/* <Subject title="React" sub="FOR UI"></Subject> */}
-        <TOC data={this.state.contents}
-          onChangePage={function(id){
-            this.setState(
-              {
-                mode:'read',
-                selected_content_id:Number(id)
-              }
-              );
-          }.bind(this)}
-        ></TOC>
-        <Control onChangeMode={function(_mode){
-          this.setState({
-            mode:_mode
-          });
-        }.bind(this)}></Control>
-        {_article}
-      </div>
+  // }
+
+  //getContent end
+
+  // render() {
+  //   console.log('App render');
+  //   return (
+  //     <div>
+  //       <Subject 
+  //         title={this.state.subject.title} 
+  //         sub={this.state.subject.sub}
+  //         onChangePage={function(){this.setState({mode:'Welcome'})}.bind(this)}
+  //       ></Subject>   {/* <Subject /> */}
+
+  //       {/* <Subject title="React" sub="FOR UI"></Subject> */}
+  //       <TOC data={this.state.contents}
+  //         onChangePage={function(id){
+  //           this.setState(
+  //             {
+  //               mode:'read',
+  //               selected_content_id:Number(id)
+  //             }
+  //             );
+  //         }.bind(this)}
+  //       ></TOC>
+  //       <Control onChangeMode={function(_mode){
+  //         this.setState({
+  //           mode:_mode
+  //         });
+  //       }.bind(this)}></Control>
+  //       {this.getContent()}
+  //     </div>
       // <div className="App">
       //   <header className="App-header">
       //     <img src={logo} className="App-logo" alt="logo" />
@@ -133,12 +154,12 @@ class App extends Component {
       //     </a>
       //   </header>
       // </div>
-    );
-  }
-}
+//     );
+//   }
+// }
 
 
-export default App;
+// export default App;
 
 //--------------------------------------------------------------------------------------------------------------------------
 
@@ -203,3 +224,142 @@ export default App;
 //   }
 // }
 // export default App;
+
+//--------------------------------------------------------------------------------------------------------------------------
+
+import './App.css';
+import React,{ Component } from 'react';
+import TOC from './components/TOC'
+import Subject from './components/Subject';
+import ReadContent from './components/ReadContent';
+import Control from './components/Control';
+import CreateContent from './components/CreateContent';
+import UpdateContent from './components/UpdateContent';
+
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.max_content_id = 3;
+    this.state = {
+      mode:'read',
+      selected_content_id:2,
+      subject:{title:'WEB',sub:'World Wid Web'},
+      welcome:{title:'Welcome',desc:'Hello, React!!'},
+      contents:[
+        {id:1,title:'HTML',desc:'HTML is for imformation'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
+      ]
+    }
+  }
+
+  //getReadContent start
+
+  getReadContent(){
+    let i = 0;
+      while(i < this.state.contents.length) {
+        let data = this.state.contents[i];
+        if(data.id === this.state.selected_content_id) {
+          return data;
+        }
+        i = i + 1;
+      }
+  }
+
+  //getReadContent end
+
+  //getContent start
+  
+  getContent(){
+    let _title, _desc = null, _article;
+    if(this.state.mode === 'welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>;
+    }else if(this.state.mode === 'read'){
+      let _content = this.getReadContent();
+      _article = <ReadContent title={_content.title} desc={_content.desc}></ReadContent>;
+    }else if(this.state.mode === 'create'){
+      _article = <CreateContent onSubmit={function(_title,_desc){
+        this.max_content_id = this.max_content_id + 1;
+        // this.state.contents.push(
+        //   {id:this.max_content_id,title:_title,desc:_desc}
+        // );
+
+        // let _contents = this.state.contents.concat(
+        //      {id:this.max_content_id,title:_title,desc:_desc}
+        //  );
+
+        let _contents = Array.from(this.state.contents);
+        _contents.push({id:this.max_content_id,title:_title,desc:_desc})
+
+        this.setState({
+          //contents : this.state.contents
+          mode:'read',
+          contents : _contents,
+          selected_content_id:this.max_content_id
+        })
+        
+      }.bind(this)}></CreateContent>
+    }else if(this.state.mode === 'update'){
+      let _content = this.getReadContent();
+      _article = <UpdateContent 
+                  data = {_content} 
+                  onSubmit={function(_id,_title,_desc){
+                    let _contents = Array.from(this.state.contents);
+                    let i = 0;
+                    while(i < _contents.length) {
+                      if(_contents[i].id === _id) {
+                        _contents[i] = {id:_id,title:_title,desc:_desc};
+                        break;
+                      }
+                      i = i + 1;
+                    }
+                    this.setState({
+                      contents:_contents
+                    })
+                  }.bind(this)}>
+                  </UpdateContent>
+    }
+
+    return _article;
+  }
+
+  //getContent end
+
+  render(){
+    console.log('App render');
+
+    return (
+      <div>
+        <Subject 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub} 
+          onChangePage={function(){
+            this.setState({mode:'welcome'}) ;
+          }.bind(this)}
+        >
+        </Subject>
+        <TOC data={this.state.contents}
+          onChangePage={function(id){
+            this.setState(
+              {
+                mode:'read',
+                selected_content_id:Number(id)
+              }
+            ) ;
+          }.bind(this)}
+        >
+        </TOC>
+        <Control onChangeMode={function(_mode){
+            this.setState({
+              mode:_mode
+            });
+          }.bind(this)}></Control>
+        {this.getContent()}
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/components/UpdateContent.js
+++ src/components/UpdateContent.js
@@ -1,23 +1,48 @@
 import React,{ Component } from 'react';
 
 class UpdateContent extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            id:this.props.data.id,
+            title : this.props.data.title,
+            desc:this.props.data.desc
+        }
+        this.inputFormHandler = this.inputFormHandler.bind(this);
+    }
+    inputFormHandler(e) {
+      this.setState({[e.target.name]:e.target.value});
+
+    }
     render() {
-      console.log('COL render');
+      console.log('UpdateContent render');
       return (
         <article>
-          <h2>Create</h2>
-            <form
-              action="/create_process"
-              method="post"
-              onSubmit={function(e){
-                e.preventDefault();
-                this.props.onSubmit(e.target.title.value,e.target.desc.value);
-                // alert('Submit');
-              }.bind(this)}
+            <h2>Update</h2>
+            <form 
+                action="/create_process" 
+                method="post" 
+                onSubmit={function(e){
+                    e.preventDefault();
+                    this.props.onSubmit(this.state.id,this.state.title,this.state.desc);
+                }.bind(this)}
             >
-              <p><input type='text' name='title' placeholder='title'></input></p>
-              <p><textarea name='desc' placeholder='description'></textarea></p>
-              <p><input type='submit'></input></p>
+                <input type='hidden' name='id' value={this.state.id}></input>
+                <p><input 
+                    type="text" 
+                    name="title" 
+                    placeholder="title"
+                    value={this.state.title}
+                    onChange={this.inputFormHandler}
+                ></input>
+                </p>
+                <p><textarea 
+                    name='desc' 
+                    placeholder='description' 
+                    value={this.state.desc}
+                    onChange={this.inputFormHandler}
+                ></textarea></p>
+                <p><input type='submit'></input></p>
             </form>
         </article>
       );
